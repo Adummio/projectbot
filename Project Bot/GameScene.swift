1750a4c6c34798386fb5dadd9a452b9e3cded702
Tony Tresgots <toni.tresgots@gmail.com>
2020-03-28 18:20:50 +0100

    fix area

--- Project Bot/GameScene.swift
+++ Project Bot/GameScene.swift
@@ -16,9 +16,13 @@ class GameScene: SKScene {
     var enemy : SKSpriteNode?
     let joystick = 🕹(withDiameter: 100)
     var arena: SKShapeNode?
+    let resultLabel = SKLabelNode(fontNamed:"Chalkduster")
+
     let arenaRadius: CGFloat = 200.0
     
     let enemySpeed:CGFloat = 3.0
+    
+    var circle = SKShapeNode()
     
     override func didMove(to view: SKView) {
         
@@ -27,10 +31,16 @@ class GameScene: SKScene {
         background.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         background.position = CGPoint(x: size.width/2, y: size.height/2)
         addChild(background)
-       
-//        let image = UIImage(named: "")
+        
+        resultLabel.isHidden = true
+        resultLabel.fontSize = 20
+        resultLabel.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        resultLabel.fontColor = UIColor.black
+        addChild(resultLabel)
+
+        //        let image = UIImage(named: "")
         joystick.handleImage = nil
-//        let substrateImage = UIImage(named: "")
+        //        let substrateImage = UIImage(named: "")
         joystick.baseImage = nil
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
@@ -40,14 +50,14 @@ class GameScene: SKScene {
         joystick.isMoveable = true
         addChild(moveJoystickHiddenArea)
         
-//        joystick.on(.begin) { [unowned self] _ in
-//            let actions = [
-//                SKAction.scale(to: 0.5, duration: 0.5),
-//                SKAction.scale(to: 1, duration: 0.5)
-//            ]
-//
-//            self.mainCharacter?.run(SKAction.sequence(actions))
-//        }
+        //        joystick.on(.begin) { [unowned self] _ in
+        //            let actions = [
+        //                SKAction.scale(to: 0.5, duration: 0.5),
+        //                SKAction.scale(to: 1, duration: 0.5)
+        //            ]
+        //
+        //            self.mainCharacter?.run(SKAction.sequence(actions))
+        //        }
         
         joystick.on(.move) { [unowned self] joystick in
             guard let mainCharacter = self.mainCharacter else {
@@ -62,17 +72,17 @@ class GameScene: SKScene {
             mainCharacter.zRotation = joystick.angular
         }
         
-//        joystick.on(.end) { [unowned self] _ in
-//            let actions = [
-//                SKAction.scale(to: 1.5, duration: 0.5),
-//                SKAction.scale(to: 1, duration: 0.5)
-//            ]
-//
-//            self.mainCharacter?.run(SKAction.sequence(actions))
-//        }
+        //        joystick.on(.end) { [unowned self] _ in
+        //            let actions = [
+        //                SKAction.scale(to: 1.5, duration: 0.5),
+        //                SKAction.scale(to: 1, duration: 0.5)
+        //            ]
+        //
+        //            self.mainCharacter?.run(SKAction.sequence(actions))
+        //        }
         
         addMainCharacter(atPosition: CGPoint(x: frame.midX, y: frame.midY))
-        addEnemy(atPosition: CGPoint(x: frame.midX, y: frame.maxY-100))
+        addEnemy(atPosition: CGPoint(x: frame.midX, y: frame.maxY-300))
         addCircle()
         
         view.isMultipleTouchEnabled = true
@@ -112,7 +122,7 @@ class GameScene: SKScene {
     }
     
     func addCircle(){
-        let circle = SKShapeNode(circleOfRadius: arenaRadius)
+        self.circle = SKShapeNode(circleOfRadius: arenaRadius)
         circle.position = CGPoint(x: frame.midX, y: frame.midY)
         circle.fillColor = .yellow
         circle.glowWidth = 1.0
@@ -122,169 +132,187 @@ class GameScene: SKScene {
         arena = circle
     }
     
-//    func addCircleObstacle() {
-//      // 1
-//      let path = UIBezierPath()
-//      // 2
-//      path.move(to: CGPoint(x: 0, y: -200))
-//      // 3
-//      path.addLine(to: CGPoint(x: 0, y: -160))
-//      // 4
-//      path.addArc(withCenter: CGPoint.zero,
-//                  radius: 160,
-//                  startAngle: CGFloat(3.0 * Double.pi/2),
-//                  endAngle: CGFloat(0),
-//                  clockwise: true)
-//      // 5
-//      path.addLine(to: CGPoint(x: 200, y: 0))
-//      path.addArc(withCenter: CGPoint.zero,
-//                  radius: 200,
-//                  startAngle: CGFloat(0.0),
-//                  endAngle: CGFloat(3.0 * Double.pi/2),
-//                  clockwise: false)
-//
-//
-//        let section = SKShapeNode(path: path.cgPath)
-//        section.position = CGPoint(x: size.width/2, y: size.height/2)
-//        section.fillColor = .yellow
-//        section.strokeColor = .yellow
-//        addChild(section)
-//    }
-
+    //    func addCircleObstacle() {
+    //      // 1
+    //      let path = UIBezierPath()
+    //      // 2
+    //      path.move(to: CGPoint(x: 0, y: -200))
+    //      // 3
+    //      path.addLine(to: CGPoint(x: 0, y: -160))
+    //      // 4
+    //      path.addArc(withCenter: CGPoint.zero,
+    //                  radius: 160,
+    //                  startAngle: CGFloat(3.0 * Double.pi/2),
+    //                  endAngle: CGFloat(0),
+    //                  clockwise: true)
+    //      // 5
+    //      path.addLine(to: CGPoint(x: 200, y: 0))
+    //      path.addArc(withCenter: CGPoint.zero,
+    //                  radius: 200,
+    //                  startAngle: CGFloat(0.0),
+    //                  endAngle: CGFloat(3.0 * Double.pi/2),
+    //                  clockwise: false)
+    //
+    //
+    //        let section = SKShapeNode(path: path.cgPath)
+    //        section.position = CGPoint(x: size.width/2, y: size.height/2)
+    //        section.fillColor = .yellow
+    //        section.strokeColor = .yellow
+    //        addChild(section)
+    //    }
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         /* Called when a touch begins */
-//        if touches.count == 2{
-//            let actions = [
-//                let vector = CGVector(
-//                SKAction.move(by: <#T##CGVector#>, duration: <#T##TimeInterval#>)
-//            ]
-//        }
+        //        if touches.count == 2{
+        //            let actions = [
+        //                let vector = CGVector(
+        //                SKAction.move(by: <#T##CGVector#>, duration: <#T##TimeInterval#>)
+        //            ]
+        //        }
     }
-
-//    var entities = [GKEntity]()
-//    var graphs = [String : GKGraph]()
-//
-//    private var lastUpdateTime : TimeInterval = 0
-//    private var label : SKLabelNode?
-//    private var spinnyNode : SKShapeNode?
-//
-//    override func sceneDidLoad() {
-//
-//        self.lastUpdateTime = 0
-//
-//        // Get label node from scene and store it for use later
-//        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-//        if let label = self.label {
-//            label.alpha = 0.0
-//            label.run(SKAction.fadeIn(withDuration: 2.0))
-//        }
-//
-//        // Create shape node to use during mouse interaction
-//        let w = (self.size.width + self.size.height) * 0.05
-//        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-//
-//        if let spinnyNode = self.spinnyNode {
-//            spinnyNode.lineWidth = 2.5
-//
-//            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-//            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-//                                              SKAction.fadeOut(withDuration: 0.5),
-//                                              SKAction.removeFromParent()]))
-//        }
-//    }
-//
-//
-//    func touchDown(atPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.green
-//            self.addChild(n)
-//        }
-//    }
-//
-//    func touchMoved(toPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.blue
-//            self.addChild(n)
-//        }
-//    }
-//
-//    func touchUp(atPoint pos : CGPoint) {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-//            n.position = pos
-//            n.strokeColor = SKColor.red
-//            self.addChild(n)
-//        }
-//    }
-//
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        if let label = self.label {
-//            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-//        }
-//
-//        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-//    }
-//
-//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-//    }
-//
-//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-//    }
-//
-//    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-//    }
-//
-//
+    
+    //    var entities = [GKEntity]()
+    //    var graphs = [String : GKGraph]()
+    //
+    //    private var lastUpdateTime : TimeInterval = 0
+    //    private var label : SKLabelNode?
+    //    private var spinnyNode : SKShapeNode?
+    //
+    //    override func sceneDidLoad() {
+    //
+    //        self.lastUpdateTime = 0
+    //
+    //        // Get label node from scene and store it for use later
+    //        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+    //        if let label = self.label {
+    //            label.alpha = 0.0
+    //            label.run(SKAction.fadeIn(withDuration: 2.0))
+    //        }
+    //
+    //        // Create shape node to use during mouse interaction
+    //        let w = (self.size.width + self.size.height) * 0.05
+    //        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+    //
+    //        if let spinnyNode = self.spinnyNode {
+    //            spinnyNode.lineWidth = 2.5
+    //
+    //            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+    //            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+    //                                              SKAction.fadeOut(withDuration: 0.5),
+    //                                              SKAction.removeFromParent()]))
+    //        }
+    //    }
+    //
+    //
+    //    func touchDown(atPoint pos : CGPoint) {
+    //        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+    //            n.position = pos
+    //            n.strokeColor = SKColor.green
+    //            self.addChild(n)
+    //        }
+    //    }
+    //
+    //    func touchMoved(toPoint pos : CGPoint) {
+    //        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+    //            n.position = pos
+    //            n.strokeColor = SKColor.blue
+    //            self.addChild(n)
+    //        }
+    //    }
+    //
+    //    func touchUp(atPoint pos : CGPoint) {
+    //        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+    //            n.position = pos
+    //            n.strokeColor = SKColor.red
+    //            self.addChild(n)
+    //        }
+    //    }
+    //
+    //    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    //        if let label = self.label {
+    //            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+    //        }
+    //
+    //        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+    //    }
+    //
+    //    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    //        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    //    }
+    //
+    //    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    //        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    //    }
+    //
+    //    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    //        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    //    }
+    //
+    //
     override func update(_ currentTime: TimeInterval) {
-//        // Called before each frame is rendered
-//
-//        // Initialize _lastUpdateTime if it has not already been
-//        if (self.lastUpdateTime == 0) {
-//            self.lastUpdateTime = currentTime
-//        }
-//
-//        // Calculate time since last update
-//        let dt = currentTime - self.lastUpdateTime
-//
-//        // Update entities
-//        for entity in self.entities {
-//            entity.update(deltaTime: dt)
-//        }
-//
-//        self.lastUpdateTime = currentTime
         
-
+        //        // Called before each frame is rendered
+        //
+        //        // Initialize _lastUpdateTime if it has not already been
+        //        if (self.lastUpdateTime == 0) {
+        //            self.lastUpdateTime = currentTime
+        //        }
+        //
+        //        // Calculate time since last update
+        //        let dt = currentTime - self.lastUpdateTime
+        //
+        //        // Update entities
+        //        for entity in self.entities {
+        //            entity.update(deltaTime: dt)
+        //        }
+        //
+        //        self.lastUpdateTime = currentTime
+        
+        
         let location = mainCharacter?.position
-//        let arenaLocation = arena?.position
-//        
-//        let xDist = (location?.x ?? 0) - (arenaLocation?.x ?? 0)
-//        let yDist = (location?.y ?? 0) - (arenaLocation?.y ?? 0)
-//        let distance = sqrt(xDist*xDist + yDist*yDist)
-//        
-//        if distance < arenaRadius{
-//            print("die")
-//        }
-
-         for enemy in enemies {
-             //Aim
-             let dx = (location?.x)! - enemy.position.x
-             let dy = (location?.y)! - enemy.position.y
-             let angle = atan2(dy, dx)
-
-             enemy.zRotation = angle - 3 * .pi/2
-
-             //Seek
-             let velocityX = cos(angle) * enemySpeed
-             let velocityY = sin(angle) * enemySpeed
-
-             enemy.position.x += velocityX
-             enemy.position.y += velocityY
-         }
         
+        if circle.contains(location!) {
+            mainCharacter!.isHidden = false
+        } else {
+            self.resultLabel.isHidden = false
+            resultLabel.text = "Porco dio you lost !"
+            mainCharacter!.isHidden = true
+            mainCharacter?.removeFromParent()
+        }
+        //        let arenaLocation = arena?.position
+        //
+        //        let xDist = (location?.x ?? 0) - (arenaLocation?.x ?? 0)
+        //        let yDist = (location?.y ?? 0) - (arenaLocation?.y ?? 0)
+        //        let distance = sqrt(xDist*xDist + yDist*yDist)
+        //
+        //        if distance < arenaRadius{
+        //            print("die")
+        //        }
+        
+        for enemy in enemies {
+            //Aim
+            let dx = (location?.x)! - enemy.position.x
+            let dy = (location?.y)! - enemy.position.y
+            let angle = atan2(dy, dx)
+            
+            enemy.zRotation = angle - 3 * .pi/2
+            
+            //Seek
+            let velocityX = cos(angle) * enemySpeed
+            let velocityY = sin(angle) * enemySpeed
+            
+            enemy.position.x += velocityX
+            enemy.position.y += velocityY
+            
+            if circle.contains(enemy.position) {
+                enemy.isHidden = false
+            } else {
+                self.resultLabel.isHidden = false
+                resultLabel.text = "Porco dio you won !"
+                enemy.isHidden = true
+                enemy.removeFromParent()
+            }
+        }
     }
 }
